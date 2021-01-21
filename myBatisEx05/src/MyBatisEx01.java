58e89a13133701b0d03b46655c19a21afa8ff1d1
김해선 <kimhaeseon1@google.com>
2021-01-21 14:16:26 +0900

    myBatis - annotation
    java project

--- myBatisEx05/src/MyBatisEx01.java
+++ myBatisEx05/src/MyBatisEx01.java
@@ -1,5 +1,7 @@
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -22,7 +24,6 @@ public class MyBatisEx01 {
 			is = Resources.getResourceAsStream( resource );
 			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
 			
-			//
 			sqlSession = sqlSessionFactory.openSession(true);
 			
 			// mapper interface 호출
@@ -34,6 +35,13 @@ public class MyBatisEx01 {
 			System.out.println(to.getDeptno());
 			System.out.println(to.getDname());
 			System.out.println(to.getLoc());
+			
+			List<DeptTO> lists = mapper.selectList();
+			for(DeptTO tto: lists) {
+				System.out.println(tto.getDeptno());
+				System.out.println(tto.getDname());
+				System.out.println(tto.getLoc());
+			}
 		} catch (IOException e) {
 			System.out.println("[에러]" + e.getMessage());
 		}finally {
--- myBatisEx05/src/config/SqlMapperInter.java
+++ myBatisEx05/src/config/SqlMapperInter.java
@@ -1,11 +1,20 @@
 package config;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Select;
 
 import model1.DeptTO;
 
 public interface SqlMapperInter {
 	
+	// overloading  -> 하지마라 
+	/*@Select("select deptno, dname, loc from dept where deptno=10") */
+	//public DeptTO selectByDeptno();
+
 	@Select("select deptno, dname, loc from dept where deptno=#{deptno}")
 	public DeptTO selectByDeptno(String deptno);
+
+	@Select("select deptno, dname, loc from dept")
+	public List<DeptTO> selectList();
 }
